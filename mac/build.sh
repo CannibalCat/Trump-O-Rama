7cc7df098ee46f58160bc76792836d17b752d85f
gubatron <gubatron@gmail.com>
2016-05-07 23:34:51 -0400

    [osx] added 1024x1024 icon

--- mac/build.sh
+++ mac/build.sh
@@ -2,8 +2,8 @@
 
 # This script will build the Mac export and make sure the TrumpORama.app has a proper Mac icon.
 
-pushd ..
-openfl build mac
-popd
+#pushd ..
+#openfl build mac
+#popd
 ./update_icon.sh
 cp icon.icns ../export/mac64/cpp/bin/TrumpORama.app/Contents/Resources/
